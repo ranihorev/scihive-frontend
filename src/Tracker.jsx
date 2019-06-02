3052e8bccef9d9342585d31835359d73539f1dc2
Rani <ranihorev@gmail.com>
2019-06-01 20:17:15 -0700

    Added sentry

--- src/Tracker.jsx
+++ src/Tracker.jsx
@@ -27,5 +27,3 @@ export const withTracker = (WrappedComponent, options = {}) => {
 
   return HOC;
 };
-
-export default withTracker;
--- src/index.jsx
+++ src/index.jsx
@@ -1,29 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import axios from 'axios';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import * as Sentry from '@sentry/browser';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import {Provider} from "react-redux";
-import {SET_USER} from "./actions";
-import axios from "axios";
-import {store} from './store';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
-import withTracker from "./Tracker";
+import { SET_USER } from './actions';
+import { store } from './store';
+import { withTracker } from './Tracker';
+import './index.css';
+
+if (process.env.REACT_APP_SENTRY_DSN) Sentry.init({ dsn: process.env.REACT_APP_SENTRY_DSN });
 
 axios.defaults.baseURL = process.env.REACT_APP_BASE_URL;
 axios.defaults.withCredentials = true;
 const user = localStorage.getItem('username');
 
-if(user) {
+if (user) {
   store.dispatch({ type: SET_USER, payload: user });
 }
 
 ReactDOM.render(
   <Provider store={store}>
     <Router>
-      <Route component={withTracker(App)}/>
+      <Route component={withTracker(App)} />
     </Router>
-  </Provider>, document.getElementById('root'));
+  </Provider>,
+  document.getElementById('root'),
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

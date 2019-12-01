0d34fe5a7404ee424e0d52becafae1eb20e2e276
Rani <ranihorev@gmail.com>
2019-12-01 08:53:49 -0800

    WIP upload file

--- src/components/uploader/UploadModal.tsx
+++ src/components/uploader/UploadModal.tsx
@@ -7,7 +7,19 @@ import { presets } from '../../utils';
 import Axios from 'axios';
 import { isEmpty } from 'lodash';
 
-export const UploadModal: React.FC<{ isOpen: boolean; closeModal: () => void }> = ({ isOpen, closeModal }) => {
+interface Author {
+  first_name: string;
+  last_name: string;
+}
+
+interface FileMetadata {
+  id: string;
+  title: string;
+  abstract: string;
+  authors: Author[];
+}
+
+const FileUpload: React.FC<{ setFileMeta: (meta: FileMetadata) => void }> = ({ setFileMeta }) => {
   const onDrop = React.useCallback<Required<DropzoneOptions>['onDrop']>(acceptedFiles => {
     if (isEmpty(acceptedFiles)) {
       return;
@@ -18,10 +30,15 @@ export const UploadModal: React.FC<{ isOpen: boolean; closeModal: () => void }> 
       headers: {
         'Content-Type': 'multipart/form-data',
       },
-    }).catch(res => {
-      console.log(res.message);
-    });
+    })
+      .then(res => {
+        setFileMeta(res.data);
+      })
+      .catch(res => {
+        console.log(res.message);
+      });
   }, []);
+
   const { getRootProps, getInputProps, isDragActive } = useDropzone({
     onDrop,
     multiple: false,
@@ -31,35 +48,43 @@ export const UploadModal: React.FC<{ isOpen: boolean; closeModal: () => void }> 
   });
 
   return (
-    <Modal open={isOpen} onClose={closeModal}>
-      <Fade in={isOpen}>
-        <div css={presets.modalCss}>
-          <div
-            {...getRootProps()}
-            css={css`
-              flex: 1;
-              display: flex;
-              flex-direction: column;
-              align-items: center;
-              padding: 20px;
-              border-width: 2px;
-              border-radius: 2px;
-              border-color: #eeeeee;
-              border-style: dashed;
-              background-color: #fafafa;
-              color: #bdbdbd;
-              outline: none;
-              transition: border 0.24s ease-in-out;
-              &:focus {
-                border-color: #2196f3;
-              }
-            `}
-          >
-            <input {...getInputProps()} />
-            <p>{isDragActive ? 'Drop file here...' : "Drag 'n' drop file here, or click to select"}</p>
-          </div>
-        </div>
-      </Fade>
-    </Modal>
+    <div
+      {...getRootProps()}
+      css={css`
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding: 20px;
+        border-width: 2px;
+        border-radius: 2px;
+        border-color: #eeeeee;
+        border-style: dashed;
+        background-color: #fafafa;
+        color: #bdbdbd;
+        outline: none;
+        transition: border 0.24s ease-in-out;
+        &:focus {
+          border-color: #2196f3;
+        }
+      `}
+    >
+      <input {...getInputProps()} />
+      <p>{isDragActive ? 'Drop file here...' : "Drag 'n' drop file here, or click to select"}</p>
+    </div>
   );
 };
+
+export const UploadModal: React.FC<{ isOpen: boolean; closeModal: () => void }> = React.memo(
+  ({ isOpen, closeModal }) => {
+    const [fileMeta, setFileMeta] = React.useState<FileMetadata | undefined>();
+    console.log(fileMeta);
+    return (
+      <Modal open={isOpen} onClose={closeModal}>
+        <Fade in={isOpen}>
+          <div css={presets.modalCss}>{!fileMeta ? <FileUpload setFileMeta={setFileMeta} /> : <div>Loaded</div>}</div>
+        </Fade>
+      </Modal>
+    );
+  },
+);
--- src/components/uploader/index.tsx
+++ src/components/uploader/index.tsx
@@ -4,12 +4,13 @@ import React from 'react';
 import { UploadButton } from './UploadFloatingButton';
 import { UploadModal } from './UploadModal';
 
-export const FileUploader: React.FC = () => {
+export const FileUploader: React.FC = React.memo(() => {
   const [isOpen, setIsOpen] = React.useState(false);
+  const closeModal = React.useCallback(() => setIsOpen(false), [setIsOpen]);
   return (
     <React.Fragment>
       <UploadButton openModal={() => setIsOpen(true)} />
-      <UploadModal isOpen={isOpen} closeModal={() => setIsOpen(false)} />
+      <UploadModal isOpen={isOpen} closeModal={closeModal} />
     </React.Fragment>
   );
-};
+});

bf7142a8a178f8d553cd3e76d01557fe954529db
Rani <ranihorev@gmail.com>
2019-12-18 20:29:55 -0800

    Convert file uploader to public

--- src/components/uploader/Warning.tsx
+++ src/components/uploader/Warning.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 
 export const Warning: React.FC = () => (
   <Typography variant="caption" component="div" css={{ marginTop: 8, marginBottom: 8 }}>
-    Upload at your own risk warning, paper might be removed, paper is not publicly searchable, links are not protect,
-    don't post on public space
+    Before deciding to upload a paper, please recall that some scientific papers have copyrights. SciHive takes no
+    responsibility for the content you upload. You should do it at your own risk.
   </Typography>
 );

c2f10a2ff3993c0f82aa16ef036bedfbfcebb857
Rani <ranihorev@gmail.com>
2020-01-15 22:27:42 -0800

    Ask users to log in to upload papers

--- src/components/uploader/index.tsx
+++ src/components/uploader/index.tsx
@@ -4,17 +4,26 @@ import React from 'react';
 import { track } from '../../Tracker';
 import { UploadButton } from './UploadFloatingButton';
 import { UploadModal } from './UploadModal';
+import { useUserStore } from '../../stores/user';
 
 export const FileUploader: React.FC = React.memo(() => {
   const [isOpen, setIsOpen] = React.useState(false);
   const closeModal = React.useCallback(() => setIsOpen(false), [setIsOpen]);
+  const { isLoggedIn, toggleLoginModal } = useUserStore(state => ({
+    isLoggedIn: Boolean(state.userData),
+    toggleLoginModal: state.toggleLoginModal,
+  }));
 
   return (
     <React.Fragment>
       <UploadButton
         openModal={() => {
-          setIsOpen(true);
-          track('startPaperUpload');
+          track('startPaperUpload', { isLoggedIn });
+          if (isLoggedIn) {
+            setIsOpen(true);
+          } else {
+            toggleLoginModal('Please log in to upload a new paper');
+          }
         }}
       />
       <UploadModal isOpen={isOpen} closeModal={closeModal} />

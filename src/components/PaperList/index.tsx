4845d61079761c3d563d4db0ea6f6e1740b84778
Rani <ranihorev@gmail.com>
2020-05-16 08:48:15 -0700

    improved loading

--- src/components/PaperList/index.tsx
+++ src/components/PaperList/index.tsx
@@ -16,6 +16,7 @@ import InfiniteScroll from '../InfiniteScroll';
 import PapersListItem from '../PapersListItem';
 import { FileUploader } from '../uploader';
 import { Title } from './Title';
+import { isMobile } from 'react-device-detect';
 
 const formControlCss = css({
   margin: '8px 8px 8px 0px',
@@ -68,33 +69,35 @@ const getSortQuery = (queryParams: Partial<RequestParams>, isLibraryOrList: bool
   );
 };
 
-const PostLoader: React.FC<{ count: number }> = React.memo(({ count }) => (
-  <React.Fragment>
-    {range(0, count).map(() => (
-      <Card
-        css={css`
-          margin: 10px 0;
-          width: 100%;
-          position: relative;
-          margin-bottom: 20px;
-        `}
-      >
-        <div css={{ padding: `28px 20px` }}>
-          <ContentLoader height={140} width={800}>
-            <rect x="0" y="0" rx="4" ry="4" width="100%" height="10" />
-            <rect x="0" y="18" rx="4" ry="4" width="35%" height="10" />
-            <rect x="0" y="46" rx="4" ry="4" width="10%" height="8" />
-            <rect x="11%" y="46" rx="4" ry="4" width="10%" height="8" />
-            <rect x="0" y="70" rx="4" ry="4" width="100%" height="1" />
-            <rect x="0" y="90" rx="4" ry="4" width="100%" height="8" />
-            <rect x="0" y="110" rx="4" ry="4" width="100%" height="8" />
-            <rect x="0" y="130" rx="4" ry="4" width="90%" height="8" />
-          </ContentLoader>
-        </div>
-      </Card>
-    ))}
-  </React.Fragment>
-));
+const PostLoader: React.FC<{ count: number }> = React.memo(({ count }) => {
+  return (
+    <React.Fragment>
+      {range(0, count).map(() => (
+        <Card
+          css={css`
+            margin: 10px 0;
+            width: 100%;
+            position: relative;
+            margin-bottom: 20px;
+          `}
+        >
+          <div css={{ padding: isMobile ? `16px` : `28px 20px` }}>
+            <ContentLoader width="100%" viewBox={`0 0 ${isMobile ? '400' : '800'} 140`}>
+              <rect x="0" y="0" rx="4" ry="4" width="100%" height="10" />
+              <rect x="0" y="18" rx="4" ry="4" width="35%" height="10" />
+              <rect x="0" y="46" rx="4" ry="4" width="10%" height="8" />
+              <rect x="11%" y="46" rx="4" ry="4" width="10%" height="8" />
+              <rect x="0" y="70" rx="4" ry="4" width="100%" height="1" />
+              <rect x="0" y="90" rx="4" ry="4" width="100%" height="8" />
+              <rect x="0" y="110" rx="4" ry="4" width="100%" height="8" />
+              <rect x="0" y="130" rx="4" ry="4" width="90%" height="8" />
+            </ContentLoader>
+          </div>
+        </Card>
+      ))}
+    </React.Fragment>
+  );
+});
 
 const PapersList: React.FC = () => {
   const { groups, inviteGroup } = useUserStore(

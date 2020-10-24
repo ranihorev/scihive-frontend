cc9a45b581770ef3f4c611814bc779cfd206c9b0
Rani <ranihorev@gmail.com>
2020-10-23 21:57:54 -0700

    No comments sections

--- src/paper/highlights/sidebar/index.tsx
+++ src/paper/highlights/sidebar/index.tsx
@@ -2,48 +2,56 @@
 import { jsx } from '@emotion/core';
 import React from 'react';
 import { useHistory } from 'react-router';
-import { isDirectHighlight, PaperJump } from '../../../models';
+import { isDirectHighlight, PaperJump, AllHighlight, T_Highlight } from '../../../models';
 import { usePaperStore } from '../../../stores/paper';
 import { createEvent } from '../../../utils';
 import { Spacer } from '../../../utils/Spacer';
 import { JUMP_TO_EVENT } from '../../../utils/useJumpToHandler';
 import { HighlightContent } from '../HighlightContent';
 import styles from './styles.module.scss';
+import { isEqual, isEmpty } from 'lodash';
+import { Typography } from '@material-ui/core';
+
+const filterHighlights = (highlights: AllHighlight[]): T_Highlight[] => {
+  // Only keep comments with text
+  return highlights.filter(isDirectHighlight).filter(h => Boolean(h.text));
+};
 
 export const SidebarComments: React.FC = () => {
-  const highlightsRef = React.useRef<{ [key: string]: React.RefObject<HTMLDivElement> }>({});
-  const highlights = usePaperStore(state => state.highlights);
+  const highlights = usePaperStore(state => filterHighlights(state.highlights), isEqual);
   const history = useHistory();
-
-  highlights.forEach(h => {
-    highlightsRef.current[h.id] = React.createRef<HTMLDivElement>();
-  });
 
   return (
     <React.Fragment>
       <div className="flex flex-col w-full">
-        {highlights.map(highlight => {
-          if (!isDirectHighlight(highlight) || !highlight.text) return null;
-          return (
-            <div key={highlight.id} className={styles.commentRoot}>
-              <HighlightContent
-                {...highlight}
-                onGoto={() => {
-                  history.push({ hash: `highlight-${highlight.id}` });
-                  document.dispatchEvent(
-                    createEvent<PaperJump>(JUMP_TO_EVENT, {
-                      id: highlight.id,
-                      area: 'paper',
-                      type: 'highlight',
-                      location: highlight.position,
-                    }),
-                  );
-                }}
-              />
-              <Spacer size={12} />
-            </div>
-          );
-        })}
+        {isEmpty(highlights) ? (
+          <Typography variant="body2" color="textSecondary">
+            No comments yet. Be the first to add one!
+          </Typography>
+        ) : (
+          highlights.map(highlight => {
+            // if () return null;
+            return (
+              <div key={highlight.id} className={styles.commentRoot}>
+                <HighlightContent
+                  {...highlight}
+                  onGoto={() => {
+                    history.push({ hash: `highlight-${highlight.id}` });
+                    document.dispatchEvent(
+                      createEvent<PaperJump>(JUMP_TO_EVENT, {
+                        id: highlight.id,
+                        area: 'paper',
+                        type: 'highlight',
+                        location: highlight.position,
+                      }),
+                    );
+                  }}
+                />
+                <Spacer size={12} />
+              </div>
+            );
+          })
+        )}
       </div>
     </React.Fragment>
   );
